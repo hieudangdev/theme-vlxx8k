64ac2c77d8afa9bdaac191f220f6ae06228650ee
hieudangdev <computermaytinh232@gmail.com>
2024-10-15 15:23:10 +0700

    fix

--- vendor/khophim8k/kho8k-core/src/Contracts/SeoInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Kho8k\Core\Contracts;
-
-interface SeoInterface
-{
-    public function generateSeoTags();
-}
--- vendor/khophim8k/kho8k-core/src/Contracts/TaxonomyInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Kho8k\Core\Contracts;
-
-interface TaxonomyInterface extends HasUrlInterface
-{
-}
